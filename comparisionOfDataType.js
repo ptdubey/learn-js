581bb31de65698ef1a8009bf290a1c7ca4b51c11
ptdubey <adityababa@gmail.com>
2024-04-09 22:14:38 +0530

    changes

---
+++ comparisionOfDataType.js
@@ -0,0 +1,13 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);// a good programmer do not compare two different dataType
+
+console.log(null >= 0);
+console.log(null < 0);
+console.log(null === 0);
+
+//=== strict check
