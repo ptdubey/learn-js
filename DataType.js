27617e66ee7ba222c6d7bec78618dad711c714dd
ptdubey <adityababa@gmail.com>
2024-04-09 20:51:03 +0530

    changes

---
+++ DataType.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code  newer version
+
+// alert("hello"); // we are using nodeJs not browser
+
+console.log(3 + 3);
+
+console.log("aditya");
+
+//number => 2 to power 53
+//bigInt
+//String=>"ad"
+//boolean=> true or false
+//null =>standlone value
+//undefined =>
+//symbol  => unique
+
+//object
+console.log(typeof "aditya");
