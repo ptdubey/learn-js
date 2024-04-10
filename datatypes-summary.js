9de1879bb0b7741ac61fc5fae6847e634b9c483e
ptdubey <adityababa@gmail.com>
2024-04-10 23:43:25 +0530

    changes

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -24,6 +24,34 @@
 		}
 		Heap(non-primitive Type){
 			>> not gives a refrance
+			>>it gives the original value of the as the refrance
 		}
 
 */
+/* 
+example of : stack >> it gives the copy of the refrance value 
+>> changes are done in copy refrance 
+
+let adityaClasses = "helloAditya Dubey";
+let AnotherClass = adityaClasses;
+AnotherClass = "Adarsh Clases";
+console.log(adityaClasses);
+console.log(AnotherClass);
+
+*/
+// ...............................................................
+// example of heap
+// heap give value of orginal refrance
+// changes are done in orginal value
+
+let usearOne = {
+  email: "aditya@gmail.com",
+  passwd: "678237823",
+};
+
+let usearTwo = usearOne;
+usearTwo.passwd = "6307176624";
+usearTwo.email = "giri@gmail.com";
+
+console.log(usearOne.email);
+console.log(usearOne.passwd);
