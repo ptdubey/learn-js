8714cda6da775fd1f755d4cfe8abc5ed340ed81e
ptdubey <adityababa@gmail.com>
2024-04-10 23:12:13 +0530

    changes

---
+++ datatypes-summary.js
@@ -0,0 +1,29 @@
+/* 1. primitive DataType(7 types)
+ 		a>string
+ 		b>number
+		c>boolean
+		d>null
+		e>symbol
+		f>BigInt
+		
+
+
+	2.Non-primitive dataType(Refrance Type)
+			a>array
+			b>object
+			c>functions
+
+	.
+	
+
+*/
+// >>>>>>>>>>>>>>>>>>>>>details about memory>>>>>>>>>>>>
+/*
+		stack(primative type){
+			>>gives a refrance
+		}
+		Heap(non-primitive Type){
+			>> not gives a refrance
+		}
+
+*/
