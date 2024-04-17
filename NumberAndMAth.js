67cc6595a066918b575f340b705f4d67d70ff4dc
ptdubey <adityababa@gmail.com>
2024-04-17 22:29:06 +0530

    new

--- NumberAndMAth.js
+++ NumberAndMAth.js
@@ -1,4 +1,29 @@
-const Score = 400;
-const balance = new Number(100);
-console.log(balance);
-console.log(typeof balance);
+// const Score = 400;
+// const balance = new Number(100);
+// console.log(balance);
+// console.log(typeof balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(6));
+
+// const otherNumber = 123.896;
+// console.log(otherNumber.toPrecision(4));
+
+// const moreNumber = 1000000;
+// console.log(moreNumber.toLocaleString("en-IN"));
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>math>>>>>>>>>>>>>>>>
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.89));
+// console.log(Math.ceil(4.896));
+// console.log(Math.floor(4.679));
+// console.log(Math.min(2, 5, 6, 2, 3, 7, 8, 9));
+
+// console.log(Math.max(2, 5, 6, 2, 3, 7, 8, 9));
+
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 1;
+const max = 6;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
